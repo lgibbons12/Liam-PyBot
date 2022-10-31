ff0ad95aaf98efad47c3f878cab0543544d465bf
lgibbons12 <lgibbons@cannonschool.org>
2022-10-31 14:16:00 -0400

    finished quizlet

--- quizlet/quiz.py
+++ quizlet/quiz.py
@@ -27,6 +27,7 @@ with open("quizlet/quizlet.json", "r") as f:
     data = json.load(f)
 
 data['score'] = 0
+data['numans'] = 0
 
 with open('quizlet/quizlet.json', 'w') as f:
         json.dump(data, f, indent=2)
@@ -35,6 +36,7 @@ with open('quizlet/quizlet.json', 'w') as f:
 #function that can take in arguments and store it in json
 @client.event
 async def on_message(message):
+    print(message.author.id)
     if message.content.startswith("addterm"):
         with open("quizlet/quizlet.json", "r") as f:
             data = json.load(f)
@@ -101,8 +103,10 @@ async def on_message(message):
             data = json.load(f)
         if data['ans'] == "a":
             data['score'] += 1
+            data['numans'] += 1
             await message.channel.send(f"Correct! You got it right! Your Score: {data['score']}")
         else:
+            data['numans'] += 1
             await message.channel.send(f"Incorrect, the correct answer was {data['ans']}")
         with open('quizlet/quizlet.json', 'w') as f:
             json.dump(data, f, indent=2)
@@ -114,8 +118,10 @@ async def on_message(message):
             data = json.load(f)
         if data['ans'] == "b":
             data['score'] += 1
+            data['numans'] += 1
             await message.channel.send(f"Correct! You got it right! Your Score: {data['score']}")
         else:
+            data['numans'] += 1
             await message.channel.send(f"Incorrect, the correct answer was {data['ans']}")
         with open('quizlet/quizlet.json', 'w') as f:
             json.dump(data, f, indent=2)
@@ -127,8 +133,10 @@ async def on_message(message):
             data = json.load(f)
         if data['ans'] == "c":
             data['score'] += 1
+            data['numans'] += 1
             await message.channel.send(f"Correct! You got it right! Your Score: {data['score']}")
         else:
+            data['numans'] += 1
             await message.channel.send(f"Incorrect, the correct answer was {data['ans']}")
         with open('quizlet/quizlet.json', 'w') as f:
             json.dump(data, f, indent=2)
@@ -140,11 +148,98 @@ async def on_message(message):
             data = json.load(f)
         if data['ans'] == "d":
             data['score'] += 1
+            data['numans'] += 1
             await message.channel.send(f"Correct! You got it right! Your Score: {data['score']}")
         else:
+            data['numans'] += 1
             await message.channel.send(f"Incorrect, the correct answer was {data['ans']}")
         with open('quizlet/quizlet.json', 'w') as f:
             json.dump(data, f, indent=2)
+    
+
+    elif message.content == "wr":
+        with open("quizlet/quizlet.json", "r") as f:
+            data = json.load(f)
+        num = random.randint(0, len(data['terms']))
+        print(num)
+        await message.channel.send(f"What is the definition of {data['terms'][num]}")
+        data['ans'] = data['definitions'][num]
+        with open('quizlet/quizlet.json', 'w') as f:
+            json.dump(data, f, indent=2)
+
+
+    elif message.content.startswith("write"):
+        with open("quizlet/quizlet.json", "r") as f:
+            data = json.load(f)
+        msg_list = message.content.split(" ")
+        msg = msg_list[1]
+        if msg == data['ans']:
+            data['score'] += 1
+            data['numans'] += 1
+            await message.channel.send(f"Correct! You got it right! Your Score: {data['score']}")
+        else:
+            data['numans'] += 1
+            await message.channel.send(f"Incorrect, the correct answer was {data['ans']}")
+        with open('quizlet/quizlet.json', 'w') as f:
+            json.dump(data, f, indent=2)
+    
+    elif message.content == "tf":
+        with open("quizlet/quizlet.json", "r") as f:
+            data = json.load(f)
+        num1 = random.randint(0, len(data['terms']))
+        num2 = random.randint(0, len(data['terms']))
+        print(num1)
+        print(num2)
+        if num1 == num2:
+            data['ans'] = "True"
+        else:
+            data['ans'] = "False"
+        await message.channel.send(f"The definition of {data['terms'][num1]} is {data['definitions'][num2]} (True or False?)")
+        with open('quizlet/quizlet.json', 'w') as f:
+            json.dump(data, f, indent=2)
+    elif message.content == "True":
+        with open("quizlet/quizlet.json", "r") as f:
+            data = json.load(f)
+        if data['ans'] == "True":
+            data['score'] += 1
+            data['numans'] += 1
+            await message.channel.send(f"Correct! You got it right! Your Score: {data['score']}")
+        else:
+            data['numans'] += 1
+            await message.channel.send(f"Incorrect, the correct answer was {data['ans']}")
+        with open('quizlet/quizlet.json', 'w') as f:
+            json.dump(data, f, indent=2)
+    elif message.content == "False":
+        with open("quizlet/quizlet.json", "r") as f:
+            data = json.load(f)
+        if data['ans'] == "False":
+            data['score'] += 1
+            data['numans'] += 1
+            await message.channel.send(f"Correct! You got it right! Your Score: {data['score']}")
+        else:
+            data['numans'] += 1
+            await message.channel.send(f"Incorrect, the correct answer was {data['ans']}")
+        with open('quizlet/quizlet.json', 'w') as f:
+            json.dump(data, f, indent=2)
+    elif message.content == "Results":
+        with open("quizlet/quizlet.json", "r") as f:
+            data = json.load(f)
+        await message.channel.send(f"You have gotten {data['score']} correct out of {data['numans']}")
+        percent = 100 * (data['score'] / data['numans'])
+        await message.channel.send(f"{percent}%")
+        if percent > 0.8:
+            await message.channel.send("You are doing great!")
+        else:
+            await message.channel.send("Do Better")
+
+
+        
+
+
+
+
+        
+        
 
 
 
