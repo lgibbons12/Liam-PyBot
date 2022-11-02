a5874d1dd730b24a2c3ef4329e3e3fc1c53c935a
lgibbons12 <112109780+lgibbons12@users.noreply.github.com>
2022-11-02 15:00:40 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-with open("quizlet.json", "r") as f:
-    data = json.load(f)
-
-num = len(data['terms'])
-
-for i in (0, len(data['terms'])):
-    print(i)
-    print(data['terms'][i])
